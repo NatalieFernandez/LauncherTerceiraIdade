08bfd4af4a9391405d72b8e2cff8fa5a74e7b3cb
Nathy Fernandez <Naahfernandez@MacBook-Pro-de-Nathy.local>
2015-09-24 23:37:15 -0300

    Open House

---
+++ app/src/main/java/br/com/nffn/launcherterceiraidade/AppDetail.java
@@ -0,0 +1,9 @@
+package br.com.nffn.launcherterceiraidade;
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
